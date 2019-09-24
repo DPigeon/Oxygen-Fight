6498278c8b59266696021d0832df910177ab4d59
DPigeon <D_Pig@encs.concordia.ca>
2019-09-24 15:04:45 -0400

    Player Idle and Swimming Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     /* 
      * Initializing all our variables
      */
@@ -15,25 +14,52 @@ public class PlayerController : MonoBehaviour
 
     bool isSwimming;
     bool isSwimmingFast;
-    bool isIdle;
 
     Animator animator;
     Rigidbody2D rigidBody2D;
     Transform spriteChild;
    
-    void Start()
-    {
-        /*
-         * We get the components made in the player's inspector
-         */
+    void Start() {
+       /*
+        * We get the components made in the player's inspector
+        */
         animator = GetComponent<Animator>();
         rigidBody2D = GetComponent<Rigidbody2D>();
         spriteChild = transform.Find("PlayerSprite");
     }
 
-    
-    void Update()
-    {
-        
+    void Update() {
+        ControlCharacter();
+
+        animator.SetBool("isSwimming", isSwimming);
+        animator.SetBool("isSwimmingFast", isSwimmingFast);
+
+    }
+
+    private void ControlCharacter() {
+        /* Speed Character */
+        float speed = isSwimmingFast ? swimFastSpeed : swimSpeed;
+
+        /* Swimming Fast */
+        if (Input.GetButtonDown("Run")) isSwimmingFast = true;
+        if (Input.GetButtonUp("Run")) isSwimmingFast = false;
+
+        isSwimming = false;
+
+        if (Input.GetButton("Left")) {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            SpriteDirection(-Vector2.right);
+            isSwimming = true;
+        }
+        if (Input.GetButton("Right")) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            SpriteDirection(Vector2.right);
+            isSwimming = true;
+        }
+    }
+
+    private void SpriteDirection(Vector2 direction) {
+        Quaternion rotation3D = direction == Vector2.right ? Quaternion.LookRotation(Vector3.forward) : Quaternion.LookRotation(Vector3.back);
+        spriteChild.rotation = rotation3D;
     }
 }
