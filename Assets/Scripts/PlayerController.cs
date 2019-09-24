fc8c65a7754c7572377f955020bc613d52ecb7b9
DPigeon <D_Pig@encs.concordia.ca>
2019-09-24 14:12:10 -0400

    Initializing Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* 
+     * Initializing all our variables
+     */
+    [SerializeField]
+    float swimSpeed;
+
+    [SerializeField]
+    float swimFastSpeed;
+
+    bool isSwimming;
+    bool isSwimmingFast;
+    bool isIdle;
+
+    Animator animator;
+    Rigidbody2D rigidBody2D;
+    Transform spriteChild;
+   
+    void Start()
+    {
+        /*
+         * We get the components made in the player's inspector
+         */
+        animator = GetComponent<Animator>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        spriteChild = transform.Find("PlayerSprite");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
