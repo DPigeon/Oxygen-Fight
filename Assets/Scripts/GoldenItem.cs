c159c0f301dc5c26357f2b12224770c266453d48
DPigeon <D_Pig@encs.concordia.ca>
2019-09-25 21:45:31 -0400

    Collision Between Player and Items
    We now have collisions when collecting an item.

---
+++ Assets/Scripts/GoldenItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenItem : MonoBehaviour
+{
+    /* 
+     * If golden item collides with Player, then we display a bar on top (shows that he has it in his bag)
+     */
+    void OnTriggerEnter2D(Collider2D collider){
+        if (collider.gameObject.name == "Player")
+        {
+            Destroy(gameObject); // Destroy the bar
+            // Decrease player speed
+            // Display bar on top as well
+            // Add to counter + 1
+        }
+    }
+}
